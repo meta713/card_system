9730376a00b22689490314cd5b240689e0e14745
meta713 <mhdomodomo@gmail.com>
2017-05-11 07:45:46 +0900

    登録、変更関係の機能の調整

--- assets/scripts/socket.js
+++ assets/scripts/socket.js
@@ -1,8 +1,80 @@
 $(function(){
-  $("#submit_btn").on("click",chk_form);
+  user_data = new Data_Format();
+  $("#submit_btn").on("click",function(){
+    //var $btn = $(this).button('loading');
+    if( chk_form() ){
+      //$btn.button('reset');
+      $(".input_data").toggle();
+      $(".confirm_data").toggle();
+      $(".btn_group").toggle();
+      $(this).hide();
+      //$("#ConfirmModal").modal("show");
+    }else{
+      //alert("no");
+      //$btn.button('reset');
+    }
+  });
+  $("#back_btn").on("click",function(){
+    $(".input_data").toggle();
+    $(".confirm_data").toggle();
+    $(".btn_group").toggle();
+    $("#submit_btn").show();
+  });
+  $("#send_btn").on("click",function(){
+    //$("#back_btn").hide();
+    var $btn = $(this).button('loading');
+    $.LoadingOverlaySetup({
+      color : "rgba(255,255,255,1)"
+    });
+    $(".regist_form").LoadingOverlay("show", {
+      image       : "Preloader_1.gif"
+    });
+    $(".side_menu").animate({
+      left : "-22%"
+    });
+    // $(".side_menu").LoadingOverlay("show", {
+    //   image       : "Preloader_1.gif"
+    // });
+    setTimeout(function(){
+      $(".regist_form").LoadingOverlay("hide");
+      $(".side_menu").animate({
+        left : "0%"
+      });
+      //$(".side_menu").LoadingOverlay("hide");
+      $btn.button('reset');
+      swal({
+        title:"Success!",
+        text: '完了しました、ホームに戻ります！',
+        type:"success"
+      }).then(
+        function(){
+          window.location.href = "?page=home";
+          console.log("this is test");
+        },
+        function(dismiss){
+          //window.location.href = "?page=home";
+          console.log("this is test");
+        }
+      );
+    },2000);
+  });
+  $("#student_number").formatter({
+    'pattern': '{{9999999999}}'
+  });
+  $("#phone_number").formatter({
+    'pattern': '{{999}}{{9999}}{{9999}}'
+  });
   var page_info = getUrlVars(location.search);
   if( page_info["page"] == "regist" || page_info["page"] == "change" ){
-    $("#alert_connecting").slideToggle("normal");
+    //$("#alert_connecting").slideToggle("normal");
+    iziToast.info({
+      id : "info_toast",
+      title: 'Info',
+      message: 'サーバと接続中です。しばらくお待ちください。',
+      target : "#alert_area",
+      timeout: false,
+      progressBar: false,
+    });
     setTimeout(function(){
       var host = "ws://localhost:9999/ws";
       var socket = new WebSocket(host);
@@ -24,70 +96,114 @@ $(function(){
              try {
                res_data = JSON.parse(msg.data);
                if(res_data["status"] == "ready"){
-                 $("#alert_ready").slideToggle("slow");
+                 //$("#alert_ready").slideToggle("slow");
+                 iziToast.success({
+                   title: 'Success',
+                   message: '準備ができました！ カードをタッチしてください。',
+                   target : "#alert_area",
+                   timeout: false,
+                   progressBar: false,
+                 });
                }else if(res_data["status"] == "ok"){
                  console.log(res_data);
+                 user_data.idm = res_data["data"]["idm"];
+                 user_data.action = res_data["action"];
                  for(key in res_data["data"]){
                    $("#"+key).val(res_data["data"][key]);
                  }
+                 //$("#user_name").attr("disabled","");
+                 //$("#student_number").attr("disabled","");
                 //  $("#user_name").val(res_data["data"]["user_name"]);
                 //  $("#student_number").val(res_data["data"]["student_number"]);
-                 $("#alert_ok").slideToggle("slow",function(){
-                   setTimeout(function(){
-                     $("#cancel_btn").slideToggle("slow");
-                     $("#cancel_btn").on("closed.bs.alert",function(){
-                       $("#alert_area").slideToggle("slow",function(){
-                         $(this).remove();
-                       });
-                     });
-                   },500);
+                //  $("#alert_ok").slideToggle("slow",function(){
+                //    setTimeout(function(){
+                //      $("#cancel_btn").slideToggle("slow");
+                //      $("#cancel_btn").on("closed.bs.alert",function(){
+                //        $("#alert_area").slideToggle("slow",function(){
+                //          $(this).remove();
+                //        });
+                //      });
+                //    },500);
+                //  });
+                 iziToast.success({
+                   title: 'Complete',
+                   message: 'カードを確認しました！ 情報を登録してください。',
+                   target : "#alert_area",
+                   timeout: false,
+                   progressBar: false,
                  });
                  //$("#alert_connecting").hide("slow");
                  //$("#alert_ready").hide("slow");
-                 $("#regist_form").slideToggle("slow");
                }else if(res_data["status"] == "timeout"){
-                 $("#alert_timeout").slideToggle("slow",function(){
-                   setTimeout(function(){
-                     $("#cancel_btn").slideToggle("slow");
-                     $("#cancel_btn").on("closed.bs.alert",function(){
-                       $("#alert_area").slideToggle("slow",function(){
-                         $(this).remove();
-                       });
-                     });
-                   },500);
+                 iziToast.error({
+                   title: 'Error',
+                   message: "タイムアウトです、もう一度最初からお願いします。",
+                   target : "#alert_area",
+                   timeout: false,
+                   progressBar: false,
                  });
+                //  $("#alert_timeout").slideToggle("slow",function(){
+                //    setTimeout(function(){
+                //      $("#cancel_btn").slideToggle("slow");
+                //      $("#cancel_btn").on("closed.bs.alert",function(){
+                //        $("#alert_area").slideToggle("slow",function(){
+                //          $(this).remove();
+                //        });
+                //      });
+                //    },500);
+                //  });
                }else if(res_data["status"] == "error"){
-                 $("#alert_fail").slideToggle("slow",function(){
-                   setTimeout(function(){
-                     $("#cancel_btn").slideToggle("slow");
-                     $("#cancel_btn").on("closed.bs.alert",function(){
-                       $("#alert_area").slideToggle("slow",function(){
-                         $(this).remove();
-                       });
-                     });
-                   },500);
+                 iziToast.error({
+                   title: 'Error',
+                   message: "エラーです。もう一度最初からお願いします。",
+                   target : "#alert_area",
+                   timeout: false,
+                   progressBar: false,
                  });
+                //  $("#alert_fail").slideToggle("slow",function(){
+                //    setTimeout(function(){
+                //      $("#cancel_btn").slideToggle("slow");
+                //      $("#cancel_btn").on("closed.bs.alert",function(){
+                //        $("#alert_area").slideToggle("slow",function(){
+                //          $(this).remove();
+                //        });
+                //      });
+                //    },500);
+                //  });
                }
              } catch (e) {
+               console.log("this is test");
                console.log(e);
-               $("#alert_fail").slideToggle("slow",function(){
-                 setTimeout(function(){
-                   $("#cancel_btn").slideToggle("slow");
-                   $("#cancel_btn").on("closed.bs.alert",function(){
-                     $("#alert_area").slideToggle("slow",function(){
-                       $(this).remove();
-                     });
-                   });
-                 },500);
+               iziToast.error({
+                 title: 'Error',
+                 message: "エラーです。もう一度最初からお願いします。",
+                 target : "#alert_area",
+                 timeout: false,
+                 progressBar: false,
                });
+              //  $("#alert_fail").slideToggle("slow",function(){
+              //    setTimeout(function(){
+              //      $("#cancel_btn").slideToggle("slow");
+              //      $("#cancel_btn").on("closed.bs.alert",function(){
+              //        $("#alert_area").slideToggle("slow",function(){
+              //          $(this).remove();
+              //        });
+              //      });
+              //    },500);
+              //  });
              }
            },500);
          }
 
          socket.onerror = function(){
            console.log("error");
-           //$("#alert_connecting").hide("slow");
-           $("#alert_fail").slideToggle("slow");
+           iziToast.error({
+             title: 'Error',
+             message: "エラーです。もう一度最初からお願いします。",
+             target : "#alert_area",
+             timeout: false,
+             progressBar: false,
+           });
          }
 
          socket.onclose = function(){
@@ -96,8 +212,14 @@ $(function(){
          }
 
        }else{
-         $("#alert_connecting").slideToggle("slow");
-         $("#alert_fail").slideToggle("slow");
+         //$("#alert_connecting").slideToggle("slow");
+         iziToast.error({
+           title: 'Error',
+           message: "エラーです。もう一度最初からお願いします。",
+           target : "#alert_area",
+           timeout: false,
+           progressBar: false,
+         });
          console.log("invalid socket");
        }
     },700);
@@ -133,10 +255,96 @@ function chk_form(){
   if(!$("#email").val()) array.push("メールアドレスを入力してください");
 
   if( array.length > 0 ){
-    $("#error_area").html("<p>" + array.join("<br>"));
+    $("#error_area").html("<p>" + array.join("<br>")).show();
     return false;
   }else{
-    $("#error_area").html("");
-    return false;
+    $("#error_area").html("").hide();
+    elem = $("#regist_form input");
+    label = $(".confirm_data");
+    for(i = 0 ; i < elem.length ; i++){
+      user_data.push($(elem[i]).val());
+      $(label[i]).text($(elem[i]).val());
+    }
+    console.log(user_data.createObj());
+    return true;
   }
+}
+
+Data_Format = function( user_name , student_number , phone_number , email , idm  , action ){
+  this.index = 0;
+  this.idm = idm;
+  this.action = action;
+  this.user_name = user_name;
+  this.student_number = student_number;
+  this.phone_number = phone_number;
+  this.email = email;
+}
+
+Data_Format.prototype.push = function( data , i ){
+  if( i == undefined ){
+    switch( this.index ){
+      case 0:{
+        this.user_name = data;
+        break;
+      }
+      case 1:{
+        this.student_number = data;
+        break;
+      }
+      case 2:{
+        this.phone_number = data;
+        break;
+      }
+      case 3:{
+        this.email = data;
+        break;
+      }
+      default:{
+        this.index = 0;
+        break;
+      }
+    }
+    this.index = ( this.index + 1 ) % 4;
+    return;
+  }else if( !isNaN( i ) ){
+    switch( i ){
+      case 0:{
+        this.user_name = data;
+        return;
+      }
+      case 1:{
+        this.student_number = data;
+        return;
+      }
+      case 2:{
+        this.phone_number = data;
+        return;
+      }
+      case 3:{
+        this.email = data;
+        return;
+      }
+      default:{
+        return;
+      }
+    }
+  }else{
+    return;
+  }
+}
+
+Data_Format.prototype.createjson = function(){
+  return JSON.stringify({
+    "action" : this.action , "idm" : this.idm ,
+    "user_name" : this.user_name , "student_number" : this.student_number ,
+    "phone_number" : this.phone_number , "email" : this.email
+  });
+}
+
+Data_Format.prototype.createObj = function(){
+  return {
+    "action" : this.action , "idm" : this.idm ,
+    "user_name" : this.user_name , "student_number" : this.student_number ,
+    "phone_number" : this.phone_number , "email" : this.email
+  };
 }
